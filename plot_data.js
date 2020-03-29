a83f9aeae0e0589c7d740d4185aa3021f0e6a85b
akkamin <aidana@hawaii.edu>
2020-03-28 16:09:53 -1000

    Update plot_data.js

--- plot_data.js
+++ plot_data.js
@@ -29,12 +29,7 @@ $(document).ready(function() {
                     addNavBarItem(dataArr[i][0]);
                 }
             }
-            jQuery(".nav-link").on('click', function(){
-                $('#islandNavigation').children().each(function (){
-                    $(this).children().removeClass('active'); 
-                });
-                $(this).addClass('active');
-            });
+
         }   
     });    
 });
